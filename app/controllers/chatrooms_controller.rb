72f2b56df5f388e7fd2b9884030a6fe1b60157d8
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-17 16:14:45 +0100

    generate chatroom controller

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,7 @@
+class ChatroomsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :chats
+  get 'chatrooms/new'
+
+  get 'chatrooms/create'
+
+  resources :chatrooms do
+    resources :chats
+  end
   root 'chats#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
