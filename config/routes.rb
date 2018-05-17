741a23322b28146eee2bf0ac20597847a57f6f80
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-17 23:52:23 +0100

    - update chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :chatrooms do
-    resources :chats
-  end
+  resources :chatrooms
+  resources :chats
   root 'chatrooms#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
