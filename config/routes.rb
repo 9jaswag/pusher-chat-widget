0d8c64e64342c20b957e7c139b31430d2a5ccd4c
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-17 14:57:12 +0100

    - create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'chats/create'
+
+  get 'chats/new'
+
+  get 'chats/index'
+
+  get 'chats/show'
+
+  get 'chats/dashboard'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
