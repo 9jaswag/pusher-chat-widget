982dce0b624593c7a54a5c7f6161acb150747805
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-17 17:19:49 +0100

    - create chatroom and get response

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,31 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatroom = Chatroom.new
+  end
+
   def new
+    @chatroom = Chatroom.new
   end
 
   def create
+    @chatroom = Chatroom.new(chatroom_params)
+
+    respond_to do |format|
+      if @chatroom.save
+        format.html { redirect_to @chatroom, notice: 'Employee was successfully created.' }
+        format.json { render :show, status: :created, location: @chatroom }
+      else
+        format.html { render :new }
+        format.json { render json: @chatroom.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  def show
+  end
+
+  private
+    def chatroom_params
+      params.require(:chatroom).permit(:email, :name)
+    end
 end
