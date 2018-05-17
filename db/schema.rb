e36de12a78c3b5321341a3bad3b8e06737c2fb25
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-17 16:14:45 +0100

    generate chatroom controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180517150537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.string "message"
+    t.string "name"
+    t.bigint "chatroom_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_chats_on_chatroom_id"
+  end
+
+  add_foreign_key "chats", "chatrooms"
 end
