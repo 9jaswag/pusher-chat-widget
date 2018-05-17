1fd55b7c7a59e9a709e1be606658113524934923
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-17 17:19:49 +0100

    - create chatroom and get response

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'chatrooms/new'
-
-  get 'chatrooms/create'
-
   resources :chatrooms do
     resources :chats
   end
-  root 'chats#index'
+  root 'chatrooms#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
