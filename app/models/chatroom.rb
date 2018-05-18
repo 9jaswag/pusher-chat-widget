97a9748bc86c57e6eb45b02910b8524a385c6c9c
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-18 15:26:42 +0100

    - update admin chatrooms realtime

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,8 +1,8 @@
 class Chatroom < ApplicationRecord
-  # after_create :notify_pusher
+  after_create :notify_pusher
   has_many :chats
 
   def notify_pusher
-    # Pusher.trigger('chat', 'new-chatroom', self.as_json)
+    Pusher.trigger('chat', 'new-chatroom', self.as_json)
   end
 end
