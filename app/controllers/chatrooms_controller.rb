07a9c0db11276d086b42bcbc5192bc85b6fcc3cc
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-18 17:53:26 +0100

    - remove unused code

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -24,16 +24,6 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     render json: @chatroom.chats
-
-    # respond_to do |format|
-    #   if @chatroom
-    #     format.html { redirect_to @chatroom }
-    #     format.json { render :show, status: :ok, location: @chatroom }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @chatroom.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   private
