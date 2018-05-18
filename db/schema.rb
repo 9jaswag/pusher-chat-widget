d7f50912beaf0713eaaf5d2af94f4d28080c5e1d
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-18 18:02:36 +0100

    - refactor chatroom model to remove unused column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517150537) do
+ActiveRecord::Schema.define(version: 20180518170117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20180517150537) do
   create_table "chatrooms", force: :cascade do |t|
     t.string "email"
     t.string "name"
-    t.string "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
